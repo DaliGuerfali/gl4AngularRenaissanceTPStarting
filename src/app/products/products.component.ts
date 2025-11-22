2ac80dc30b94bd92db4d433ea26871626e2ad6a8
Dali Guerfali <daliguerfali@gmail.com>
2025-11-22 21:59:54 +0100

    completed the products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,13 @@
 import { Component } from "@angular/core";
-import {
-  BehaviorSubject,
-  Observable,
-  concatMap,
-  map,
-  takeWhile,
-  scan,
-} from "rxjs";
+import { Observable, Subject, concatMap, map, scan, shareReplay, startWith, takeWhile } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
-import { Settings } from "./dto/product-settings.dto";
+
+interface ProductsViewModel {
+  products: Product[];
+  total: number;
+  hasMore: boolean;
+}
 
 @Component({
   selector: "app-products",
@@ -17,8 +15,39 @@ import { Settings } from "./dto/product-settings.dto";
   styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent {
-  /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
-  /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
-  constructor() {}
+  private readonly pageSize = 12;
+  private readonly loadMoreTrigger$ = new Subject<void>();
+
+  private readonly vmSource$ = this.loadMoreTrigger$.pipe(
+    startWith(void 0),
+    concatMap((_, index) =>
+      this.productService.getProducts({
+        limit: this.pageSize,
+        skip: index * this.pageSize,
+      })
+    ),
+    scan<ReturnType<ProductService["getProducts"]> extends Observable<infer R> ? R : never, ProductsViewModel>(
+      (state, response) => {
+        const products = [...state.products, ...response.products];
+        return {
+          products,
+          total: response.total,
+          hasMore: products.length < response.total,
+        };
+      },
+      { products: [], total: 0, hasMore: true }
+    ),
+    takeWhile((state) => state.hasMore, true),
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
+
+  readonly vm$: Observable<ProductsViewModel> = this.vmSource$;
+  readonly products$: Observable<Product[]> = this.vm$.pipe(map((state) => state.products));
+  readonly canLoadMore$: Observable<boolean> = this.vm$.pipe(map((state) => state.hasMore));
+
+  constructor(private readonly productService: ProductService) {}
+
+  onShowMore(): void {
+    this.loadMoreTrigger$.next();
+  }
 }
